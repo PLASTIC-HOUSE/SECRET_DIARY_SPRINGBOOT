b1439a4b134dbf91271c88acc55e263451069461
이승윤 <yyuunn17@naver.com>
2022-05-12 10:56:41 +0900

    init:

---
+++ src/main/java/com/plastichouse/SECRET_DIARY_SPRINGBOOT/SecretDiarySpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.plastichouse.SECRET_DIARY_SPRINGBOOT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretDiarySpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretDiarySpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plastichouse/SECRET_DIARY_SPRINGBOOT/SecretDiarySpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plastichouse.SECRET_DIARY_SPRINGBOOT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretDiarySpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
