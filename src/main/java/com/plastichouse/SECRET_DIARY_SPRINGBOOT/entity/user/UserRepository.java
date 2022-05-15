7e8b6bcb63a7fa0442594f0f6d10f4e2d8a92c79
이승윤 <yyuunn17@naver.com>
2022-05-15 21:32:10 +0900

    ⚡️ :: findByNickname 추가

--- src/main/java/com/plastichouse/SECRET_DIARY_SPRINGBOOT/entity/user/UserRepository.java
+++ src/main/java/com/plastichouse/SECRET_DIARY_SPRINGBOOT/entity/user/UserRepository.java
@@ -2,5 +2,8 @@ package com.plastichouse.SECRET_DIARY_SPRINGBOOT.entity.user;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Integer> {
+    Optional<User> findByNickname(String nickname);
 }
