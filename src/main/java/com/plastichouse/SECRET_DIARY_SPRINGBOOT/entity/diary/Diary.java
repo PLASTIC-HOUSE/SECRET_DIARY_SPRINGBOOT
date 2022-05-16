a2db2f9e7fac1eb689c5d761ffaad6ff7b7b1167
이승윤 <yyuunn17@naver.com>
2022-05-16 22:37:24 +0900

    📑 :: Diary 생성 메소드 추가

--- src/main/java/com/plastichouse/SECRET_DIARY_SPRINGBOOT/entity/diary/Diary.java
+++ src/main/java/com/plastichouse/SECRET_DIARY_SPRINGBOOT/entity/diary/Diary.java
@@ -5,6 +5,8 @@ import com.plastichouse.SECRET_DIARY_SPRINGBOOT.entity.user.User;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -12,6 +14,7 @@ import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 @Entity
@@ -24,10 +27,21 @@ public class Diary extends BaseIdEntity {
     @Column(length = 450)
     private String content;
 
+    @DateTimeFormat(pattern = "yyyy/MM/dd`T`HH:mm:ss")
+    @CreatedDate
+    @Column(nullable = false)
     private LocalDateTime createdAt;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
+
+    public static Diary createDiary(String title, String content, User user) {
+        Diary diary = new Diary();
+        diary.title = title;
+        diary.content = content;
+        diary.user = user;
+        return diary;
+    }
 
 }
