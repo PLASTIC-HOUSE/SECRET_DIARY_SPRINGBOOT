a2d4d50220a4bbf3646bfa2b77d5b9dba79a7142
이승윤 <yyuunn17@naver.com>
2022-06-22 19:31:56 +0900

    🐛 :: @EnableJpaAuditing

--- src/main/java/com/plastichouse/SECRET_DIARY_SPRINGBOOT/SecretDiarySpringbootApplication.java
+++ src/main/java/com/plastichouse/SECRET_DIARY_SPRINGBOOT/SecretDiarySpringbootApplication.java
@@ -2,7 +2,9 @@ package com.plastichouse.SECRET_DIARY_SPRINGBOOT;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SecretDiarySpringbootApplication {
 
